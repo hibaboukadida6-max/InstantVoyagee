b32b45e2193a4a5afc88bb9227a8479d5d5a6ae2
Hiba <hibaboukadida6@gmail.com>
2026-08-11 23:32:00 +0100

    Fix React Router and production app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,77 +1,73 @@
-import UploadPassport from "./pages/UploadPassport";
 import { Routes, Route } from "react-router-dom";
 
 import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
-import ClientDetails from "./pages/ClientDetails";
+
 import Dashboard from "./pages/Dashboard";
 import Clients from "./pages/Clients";
-import Payments from "./pages/Payments";
-import Documents from "./pages/Documents";
-import EditClient from "./pages/EditClient";
-import Settings from "./pages/Settings";
 import AddClient from "./pages/AddClient";
+import ClientDetails from "./pages/ClientDetails";
+import EditClient from "./pages/EditClient";
+
+import Payments from "./pages/Payments";
+import AddPayment from "./pages/AddPayment";
+
+import Documents from "./pages/Documents";
+import Settings from "./pages/Settings";
+
+import UploadPassport from "./pages/UploadPassport";
+
 import Reservations from "./pages/Reservations";
 import AddReservation from "./pages/AddReservation";
 import ReservationDetails from "./pages/ReservationDetails";
-import AddPayment from "./pages/AddPayment";
+
 export default function App() {
   return (
-    <div className="flex bg-gray-100 min-h-screen">
-      {/* Sidebar */}
+    <div className="flex min-h-screen bg-gray-100">
       <Sidebar />
 
-      {/* Main Content */}
-      <div className="flex flex-col flex-1">
-
-        {/* Header */}
+      <div className="flex flex-1 flex-col">
         <Header />
 
-        {/* Pages */}
-        <main className="flex-1 p-8 overflow-auto">
+        <main className="flex-1 overflow-auto p-8">
           <Routes>
+            {/* Dashboard */}
             <Route path="/" element={<Dashboard />} />
+
+            {/* Clients */}
             <Route path="/clients" element={<Clients />} />
+            <Route path="/clients/add" element={<AddClient />} />
+            <Route path="/clients/edit/:id" element={<EditClient />} />
+            <Route path="/clients/:id" element={<ClientDetails />} />
+
+            {/* Payments */}
             <Route path="/payments" element={<Payments />} />
+            <Route path="/payments/add" element={<AddPayment />} />
+
+            {/* Documents */}
             <Route path="/documents" element={<Documents />} />
+
+            {/* Passport */}
+            <Route path="/passport" element={<UploadPassport />} />
+
+            {/* Reservations */}
+            <Route path="/reservations" element={<Reservations />} />
+            <Route
+              path="/reservations/add"
+              element={<AddReservation />}
+            />
+            <Route
+              path="/reservations/:id"
+              element={<ReservationDetails />}
+            />
+
+            {/* Settings */}
             <Route path="/settings" element={<Settings />} />
-            <Route path="/add-client" element={<AddClient />} 
- path="/clients/add" 
- element={<AddClient />} 
-/>
-<Route 
- path="/clients/:id" 
- element={<ClientDetails />} 
-/>
-<Route 
- path="/payments/add" 
- element={<AddPayment />} 
-/>
-<Route
-  path="/passport"
-  element={<UploadPassport />}
-/>
-<Route
-  path="/reservations"
-  element={<Reservations />}
-/>
-<Route
-path="/reservations/add"
-element={<AddReservation />}
-/>
-<Route
-  path="/reservations/:id"
-  element={<ReservationDetails />}
-/>
-<Route
-  path="/clients/edit/:id"
-  element={<EditClient />}
-/>
 
-
+            {/* Compatibilité ancienne URL */}
+            <Route path="/add-client" element={<AddClient />} />
           </Routes>
         </main>
-
       </div>
     </div>
   );
